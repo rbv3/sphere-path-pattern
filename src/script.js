b09f82eaff48d6e3273f546f06bd67b259125300
Rodrigo Valenca <rvalenca@amazon.com>
2023-09-24 17:49:30 -0300

    add boxes

--- src/script.js
+++ src/script.js
@@ -19,7 +19,19 @@ debugObject.createSphere = () => {
     }
     createSphere(radius, position)
 }
+debugObject.createBox = () => {
+    const width = Math.random() * 0.5
+    const height = Math.random() * 0.5
+    const depth = Math.random() * 0.5
+    const position = {
+        x: (Math.random() - 0.5) * 3,
+        y: 3,
+        z: (Math.random() - 0.5) * 3
+    }
+    createBox(width, height, depth, position)
+}
 gui.add(debugObject, 'createSphere')
+gui.add(debugObject, 'createBox')
 
 /**
  * Base
@@ -168,6 +180,7 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 */
 const objectsToUpdate = []
 
+// create Spheres
 const sphereGeometry = new THREE.SphereGeometry(1, 20, 20)
 const sphereMaterial = new THREE.MeshStandardMaterial({
     metalness: 0.3,
@@ -192,10 +205,51 @@ const createSphere = (radius, position) => {
         mass: 1,
         position: new CANNON.Vec3(0, 0, 0),
         shape,
-        material: defaultMaterial
     })
     body.position.copy(position)
     
+    world.addBody(body)
+
+    objectsToUpdate.push({
+        mesh,
+        body
+    })
+}
+
+// create Boxes
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
+const boxMaterial = new THREE.MeshStandardMaterial({
+    metalness: 0.3,
+    roughness: 0.4,
+    envMap: environmentMapTexture
+})
+const createBox = (width, height, depth, position) => {
+    // Three mesh
+    const mesh = new THREE.Mesh(
+        boxGeometry,
+        boxMaterial
+    )
+    mesh.scale.set(width, height, depth)
+    mesh.castShadow = true
+    mesh.position.copy(position)
+    
+    scene.add(mesh)
+
+    // Cannon body
+    const shape = new CANNON.Box(
+        new CANNON.Vec3(
+            width * 0.5,
+            height * 0.5,
+            depth * 0.5
+        )
+    )
+    const body = new CANNON.Body({
+        mass: 1,
+        position: new CANNON.Vec3(0, 0, 0),
+        shape,
+    })
+    body.position.copy(position)
+
     world.addBody(body)
 
     objectsToUpdate.push({
@@ -226,6 +280,9 @@ const tick = () =>
     for(const object of objectsToUpdate) {
         object.mesh.position.copy(
             object.body.position
+        )
+        object.mesh.quaternion.copy(
+            object.body.quaternion
         )
     }
 
